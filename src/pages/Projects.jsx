665cf3d69a4062515e0457993aacd1207d1e700c
Akram Affou <97305449+akram12affou@users.noreply.github.com>
2023-11-25 17:36:43 +0100

    ''change

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -7,6 +7,10 @@ import coinP1 from '../assets/project1_1.png'
 import coinP2 from '../assets/project1_2.png'
 import coinP3 from '../assets/project1_3.png'
 import coinP4 from '../assets/project1_4.png'
+import TRENDIFY1 from '../assets/project2_1.png'
+import TRENDIFY2 from '../assets/project2_2.png'
+import TRENDIFY3 from '../assets/project2_3.png'
+import TRENDIFY4 from '../assets/project2_4.png'
 import { motion } from "framer-motion";
 
 function Projects() {
@@ -51,22 +55,66 @@ function Projects() {
       <div className='lg:w-9/12 my_shadow h-96'>
       <Carousel >
                   <Carousel.Item>
-                    <img  className='rounded-lg w-full h-96 bg-slate-500' src={coinP1} alt="" />
+                    <img  className='rounded-lg w-full h-96 bg-slate-500 object-cover	' src={coinP1} alt="" />
                   </Carousel.Item>
                   <Carousel.Item>
-                    <img   className='rounded-lg w-full h-96 bg-slate-500' src={coinP2} alt="" />
+                    <img   className='rounded-lg w-full h-96 bg-slate-500 object-cover	' src={coinP2} alt="" />
                   </Carousel.Item>
                   <Carousel.Item>
-                    <img className='rounded-lg w-full h-96 bg-slate-500' src={coinP3} alt="" />
+                    <img className='rounded-lg w-full h-96 bg-slate-500 object-cover	' src={coinP3} alt="" />
                   </Carousel.Item>
                   <Carousel.Item>
-                    <img  src={coinP4} alt="" className='rounded-lg w-full h-96 bg-slate-500'/>
+                    <img  src={coinP4} alt="" className='rounded-lg w-full h-96 bg-slate-500 object-cover	'/>
                   </Carousel.Item>          
       </Carousel>
       </div>
 
     </div>
+    <div className="flex flex-col lg:flex-row justify-center  text-white w-10/12 mx-auto items-start gap-2 mt-4">
+      <div className="lg:w-7/12">
+        <div className="flex items-center gap-3 ">
+          <h3 className="font-semibold tracking-wider text-2xl hover:underline cursor-pointer">TRENDIFY</h3>
+          <div className='flex  items-center gap-3 mb-2 sm:hidden'>
+           <LuGithub className=" cursor-pointer  text-md hover:text-sky-300 trans hover:scale-105" />
+          <FaEye className=" cursor-pointer  text-md hover:text-sky-300 trans hover:scale-105" />
+          <FaYoutube className=" cursor-pointer text-md hover:text-sky-300 trans hover:scale-105"/> 
+          </div>
+          
+        </div>
+        <div className="flex flex-col gap-3">
+         <span className="text-sm sm:text-base tracking-wide">APEX is built with <span className='tech '>React</span> , <span className='tech'>TypeScript</span> , <span className='tech'>Redux</span> , <span className='tech'>Sass</span>, <span className='tech'>Material Ui</span>   brings you up-to-date information on every coin through an API, complete with detailed charts using <span className='tech'>Chart.js</span> . You can also track your favorite coins through authentication to make informed investment decisions with <span className='tech'> Firebase </span>.</span> 
+          <div className="hidden sm:flex  justify-evenly">
+            <a href="https://github.com/akram12affou/coingecko-app" target="_blank" className="flex items-center gap-1 Inconsolata routes_color  font-semibold cursor-pointer hover:text-sky-300 hover:tracking-wide trans w-fit text_decoration_none">
+             <LuGithub className="text-white cursor-pointer text-lg " /> Github 
+            </a>
+            <a href="https://deft-gumption-340eb5.netlify.app/" target="_blank" className="flex trans items-center gap-1 Inconsolata routes_color  font-semibold cursor-pointer  hover:tracking-wide hover:text-sky-300 w-fit text_decoration_none object-cover	">
+            <FaEye className="text-white cursor-pointer text-lg" /> Preview 
+            </a>
+            <div className="flex items-center gap-1 trans Inconsolata routes_color  font-semibold cursor-pointer  hover:tracking-wide hover:text-sky-300 w-fit">
+            <FaYoutube className="text-white cursor-pointer text-lg object-cover	" target="_blank"/> Video
+            </div>
+          </div>
+      
+          </div>
+      </div>
+      <div className='lg:w-9/12 my_shadow h-96'>
+      <Carousel >
+                  <Carousel.Item>
+                    <img  className='rounded-lg w-full h-96 bg-slate-500 object-cover	'  src={TRENDIFY1} alt="" />
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img   className='rounded-lg w-full h-96 bg-slate-500 object-cover	' src={TRENDIFY2} alt="" />
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img className='rounded-lg w-full h-96 bg-slate-500 object-cover	' src={TRENDIFY3} alt="" />
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img  src={TRENDIFY4} alt="" className='rounded-lg w-full h-96 bg-slate-500 object-cover	'/>
+                  </Carousel.Item>          
+      </Carousel>
+      </div>
 
+    </div>
     </div>
     </motion.div>
     </div>
